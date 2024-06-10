6ae67c9d88646cd0b7c4eda46bbfdc54d58132ea
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-11 01:32:45 +0300

    created a header and footer component in components folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-		my app
+		<Header/>
+		<main>
+			<h1>my app</h1>
+		</main>
+		<Footer/>
     </div>
   );
 }
