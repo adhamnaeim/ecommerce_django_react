01e5b7f14f42fbbae2d832dc34924725ac0c15ac
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-12 00:24:43 +0300

    added necessary import to ProductScreen component. passing params using UseParams in router-dom. NOTE: older method using match is deprecated based on the following link on react-router-dom v6

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link, useParams, } from 'react-router-dom'
+import { Row,Col,Image, ListGroup, Button, Card, } from 'react-bootstrap'
+
+import Rating from '../components/Rating'
+import products from '../products'
 
 function ProductScreen() {
+  const { id } = useParams();
+  const product = products.find((p) => p._id == id)
+  console.log(product)
   return (
     <div>
-      product screen test
+      {product.name}
     </div>
   )
 }
