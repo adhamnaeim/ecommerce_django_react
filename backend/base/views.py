2eead65bb2785773fee100958f2975cfcd58d7b1
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-12 22:25:56 +0300

    created a view to fetch products from dummy static data, created a urlpattern for getProducts view, changed the base app urlpattern to api/ instead of the root

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 
 def getRoutes(Request):
     return JsonResponse('hello world!', safe=False)
 
+def getProducts(Request):
+    return JsonResponse(products,safe=False)
