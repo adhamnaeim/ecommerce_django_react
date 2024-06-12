e850743e467234ce86ec8e8fcd3493665f8862c5
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-12 22:31:28 +0300

    the getProducts gets wrapped in a DRF decorator and called using the DRF response

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 
 def getRoutes(Request):
     return JsonResponse('hello world!', safe=False)
 
+@api_view(['GET'])
 def getProducts(Request):
-    return JsonResponse(products,safe=False)
+    return Response(products)
