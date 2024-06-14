2c6871bc6b533c42576e3dc11f504feab3744ae1
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-14 22:55:43 +0300

    created a serializer for the Product model, made the products views fetch data from the Product and serialize it before returning it

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,21 +3,22 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
 from .products import products
+from .serializer import ProductSerializer
 
 def getRoutes(Request):
     return JsonResponse('hello world!', safe=False)
 
 @api_view(['GET'])
 def getProducts(Request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(Request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    product = Product.objects.get(_id = pk)
+    serializer = ProductSerializer(product,many=False)
         
-    return Response(product )
+    return Response(serializer.data)
