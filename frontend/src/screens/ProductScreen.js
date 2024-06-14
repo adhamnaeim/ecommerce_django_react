649723fbc41288364b96219d8e7733b14de2c91f
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-14 03:26:54 +0300

    removed the static products.js file that housed the static data in react since data is getting pulled from django (still a static file tho in django). removed imports for such file in HomeScreen and ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,6 @@ import { Link, useParams, } from 'react-router-dom'
 import { Row,Col,Image, ListGroup, Button, Card, } from 'react-bootstrap'
 
 import Rating from '../components/Rating'
-import products from '../products'
 import axios from 'axios'
 
 function ProductScreen() {
