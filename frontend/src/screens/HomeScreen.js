c7053d73fae8a997aa5316a4d1e83d48c452b398
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-16 03:30:20 +0300

    created a component for the loading animation (Spinner) using react bootstrap and an error message (Alert) using react bootstrap. imported them in homeScreen component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useState,useEffect, } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 function HomeScreen() {
@@ -18,8 +20,8 @@ function HomeScreen() {
   return (
     <div>
       <h1>Latest products</h1>
-      {loading ? <h2>Loading...</h2> 
-        : error ? <h2>error: {error}</h2> 
+      {loading ? <Loader/>
+        : error ? <Message variant='danger'>{`error: ${error}`}</Message> 
         : <Row>
             {products.map(product => (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
