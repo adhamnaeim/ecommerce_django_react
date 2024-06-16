0339b052778fe29122f5ef21592b2d56cfcea7fd
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-16 03:08:43 +0300

    pulling data inside of the homeScreen component through productAction's listProducts. changed the structure of the component to rely on the productAction attributes (loading and error) and displaying the data

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,10 +12,10 @@ export const productListReducer = (state ={ products:[] },action) => {
             return{ loading:true, products:[] }
         
         case PRODUCT_LIST_SUCCESS:
-            return{ loading:true, products:action.payload }
+            return{ loading:false, products:action.payload }
 
         case PRODUCT_LIST_FAIL:
-            return{ loading:true, error:action.payload }
+            return{ loading:false, error:action.payload }
         
         default:
             return state
