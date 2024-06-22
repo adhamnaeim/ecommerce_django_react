9b01a3959dab17ac5d5dd1f6dfd2f6cafa1c3718
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-22 22:24:57 +0300

    organized and divided both the urls and the views to 3 categories (urls,products,orders) created folders for views and urls

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from django.contrib.auth.models import User
 
-from .models import Product
-from .products import products
-from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializer import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -49,10 +45,6 @@ def registerUser(Request):
         message = {'detail':'user with this email already exist'}
         return Response(message,status=status.HTTP_400_BAD_REQUEST)
 
-def getRoutes(Request):
-    return JsonResponse('hello world!', safe=False)
-
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(Request):
@@ -65,17 +57,4 @@ def getUserProfile(Request):
 def getUsers(Request):
     user = User.objects.all()
     serializer = UserSerializer(user,many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProducts(Request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(Request, pk):
-    product = Product.objects.get(_id = pk)
-    serializer = ProductSerializer(product,many=False)
-        
     return Response(serializer.data)
