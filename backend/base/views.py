c04a64c53778a09271d494ab1411fff7f0a6ddff
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-22 18:07:01 +0300

    created 2 userSerializers the other one is inherited with an extra serialized method for the refresh token. created a view to GetUserProfile that uses the userSerializer to fetched the predetermined data. created a url path to generate said tokens

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,25 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        serializer = UserSerializerWithToken(self.user).data
+        
+        for k,v in serializer.items():
+            data[k] = v
+        
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 def getRoutes(Request):
     return JsonResponse('hello world!', safe=False)
@@ -14,6 +32,12 @@ def getRoutes(Request):
 def getProducts(Request):
     products = Product.objects.all()
     serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getUserProfile(Request):
+    user = Request.user
+    serializer = UserSerializer(user,many=False)
     return Response(serializer.data)
 
 @api_view(['GET'])
