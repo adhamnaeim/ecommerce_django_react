3e477b70528c797be916cce26247dda7c99ff0a1
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-29 16:16:47 +0300

    Add dynamic userInfo name and logout button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
 import { LinkContainer, } from 'react-router-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 function Header() {
+
+  const userLogin = useSelector(state => state.userLogin)
+  const {userInfo} = userLogin
+  const dispatch = useDispatch()
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
   return (
     <header>
         <Navbar expand="lg" className="light" variant='dark'>
@@ -14,7 +23,17 @@ function Header() {
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
                 <LinkContainer to='/cart'><Nav.Link><i className='fas fa-shopping-cart'> Cart</i></Nav.Link></LinkContainer>
-                <LinkContainer to='/login'><Nav.Link><i className='fas fa-user'> Login</i></Nav.Link></LinkContainer>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name ? userInfo.name : userInfo.username} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                  </NavDropdown>
+                ): (
+                  
+                  <LinkContainer to='/login'><Nav.Link><i className='fas fa-user'> Login</i></Nav.Link></LinkContainer>
+                )}
             </Nav>
             </Navbar.Collapse>
         </Container>
