4a3d9af63960ab3382e673e7829087492fec4221
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-29 16:28:55 +0300

    Create constants for user register & Import to userActions & userReducers

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
 } from '../constants/UserConstants'
 
 export const login = (email,password) => async (dispatch) => {
