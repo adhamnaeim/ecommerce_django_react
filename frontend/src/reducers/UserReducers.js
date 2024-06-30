3cc285cfe94a28a66c5514ff2965a768c31d1a30
adhamnaeim <adhamnaeimmansour@gmail.com>
2024-06-30 06:31:23 +0300

    Create UserReducer -constants -> reducer -> store -> action -> screen

--- frontend/src/reducers/UserReducers.js
+++ frontend/src/reducers/UserReducers.js
@@ -6,6 +6,9 @@ import {
     USER_REGISTER_FAIL,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
  } from '../constants/UserConstants'
 
 export const UserLoginReducer = (state ={ },action) => {
@@ -43,6 +46,24 @@ export const UserRegisterReducer = (state ={ },action) => {
 
         case USER_LOGOUT:
             return{ }
+        
+        default:
+            return state
+
+    }
+}
+
+export const UserDetailsReducer = (state ={ },action) => {
+    switch(action.type){
+
+        case USER_DETAILS_REQUEST:
+            return{...state, loading:true }
+        
+        case USER_DETAILS_SUCCESS:
+            return{ loading:false, user:action.payload }
+
+        case USER_DETAILS_FAIL:
+            return{ loading:false, error:action.payload }
         
         default:
             return state
